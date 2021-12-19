1e8892c1c2ae576af7db00e7d21aa24e350d9c86
caldwelljt2 <caldwelljt@airboxinc.com>
2021-12-19 16:13:23 -0500

    restructured DrumPad and KeyBankContext to seperate files

---
+++ src/KeyBankContext.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line
+import React, { useState, useEffect, useRef, useContext, createContext } from 'react';
+
+const KeyBankContext = createContext(null)
+
+export default KeyBankContext;
