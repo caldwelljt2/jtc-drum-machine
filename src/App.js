7cd317db18b381cf1314378e95c581578a45abb9
caldwelljt2 <caldwelljt@airboxinc.com>
2021-12-19 12:38:31 -0500

    restructured most components, removed and added a number of commented log debuggers, working on event triggers.

--- src/App.js
+++ src/App.js
@@ -1,95 +1,94 @@
 // eslint-disable-next-line
-import React, { useState, useEffect } from 'react';
-// import logo from './logo.svg';
+import React, { useState, useEffect, useRef, useContext, createContext } from 'react';
 import './App.scss';
 // eslint-disable-next-line
 import { bankOne, bankTwo } from './KeybankData'
 
 
+
+const KeyBankContext = createContext(null)
+
 function App() {
-  let KeyDatabase = []
-  for (let i in bankOne) {
-    KeyDatabase.push(i)
+
+  const [currentBank, setCurrentBank] = useState(bankOne)
+
+  let letterList = []
+  for (let keyLetter in bankOne) {
+    letterList.push(keyLetter)
   }
-  // const displayThis = KeyDatabase.map((key) => {return <h1>{key}</h1>})
-  const drumPads = KeyDatabase.map((key) => { return <DrumPad padName={key} /> })
-  // const drumPads = bankOne.length
-  // console.log(KeyDatabase)
-  // .map((item) => {return <DrumPad padName={bankOne.A.keyTrigger} />})
+  const DrumPads = letterList.map((letter) => { return <DrumPad padLetter={letter} /> })
   return (
+    <KeyBankContext.Provider value={{currentBank, setCurrentBank}}>
     <div className="App">
       <header className="drum-machine" id="drum-machine">
         <div id="display">
           <h1>Drum Kit on-line by JTC</h1>
           <p>hit buttons to play or hit keyboard keys if you have them</p>
         </div>
-        {/* <h1>{bankOne.A.id}</h1> */}
         <div id="drum-buttons">
-          {drumPads}
+          {DrumPads}
+          {/* <DrumPad padLetter="A" /> */}
+          <button onClick={() => setCurrentBank(bankOne)}>Bank1</button>
+          <button onClick={() => setCurrentBank(bankTwo)}>Bank2</button>
         </div>
-        {/* <DrumPad padName={bankOne.A.keyTrigger} /> */}
-        {/* <DrumPad padName="Q" />
-        <DrumPad padName="W" />
-        <DrumPad padName="E" />
-        <DrumPad padName="A" />
-        <DrumPad padName="S" />
-        <DrumPad padName="D" />
-        <DrumPad padName="Z" />
-        <DrumPad padName="X" />
-        <DrumPad padName="C" /> */}
       </header>
     </div>
+    </KeyBankContext.Provider>
   );
 }
 
 const DrumPad = (props) => {
-  // componentDidMount() {}
-  // componentWillUnmount() {}
 
-  const handleSoundPlay = (event) => {
-    // console.log(bankOne[props.padName].url)
-    // console.log(event)
-    // console.log(props.padName)
-    // console.log(this)
-    const mySound = new Audio(bankOne[props.padName].url)
+  //  const [test, testUp] = useState(0)
+
+  //  let testUp = testUp + 1
+  const {currentBank, setCurrentBank} = useContext(KeyBankContext)
+
+  const handleSoundPlay = (event) => { // removed padLetter here temp
+    console.log(event, props.padLetter)
+    console.log(currentBank[props.padLetter])
+    const mySound = new Audio(currentBank[props.padLetter].url)
     mySound.play()
   }
 
+  const handleKeyDown = (event) => {
+    if (event.keyCode === currentBank[props.padLetter].keyCode) {
+      console.log('you hit ' + props.padLetter)
+      console.log('currentBank is currently ');
+      console.log(currentBank)  // STOPPED HERE, this is not updating
+      handleSoundPlay(props.padLetter)
+    } else {
+      console.log('keypress ignored')
+    }
+  }
+
   useEffect(() => {
-    // console.log('redraw')
-    // console.log(window.document)
-    // console.log(document)
-    document.addEventListener('keydown', function clickIt(event) {
-      // console.log(event.keyCode, props.padName, bankOne[props.padName].keyCode)
-      if (event.keyCode === bankOne[props.padName].keyCode) {
-        console.log('you hit ' + props.padName)
-        handleSoundPlay()
-      }
-    })
-    // const mySound = new Audio("https://assets.coderrocketfuel.com/pomodoro-times-up.mp3")
-    // mySound.play()
-
-    // event listener high (i.e. window.addEventListener('resize', handleResize))
-
-    // return () => {
-    //   console.log('attempting to unmount hooks')
-    //   document.addEventListener("keydown", function clickIt() {
-    //     document.removeEventListener("keydown", clickIt, false);
-    // }, false);
-
-    // return document.removeEventListener('keydown', clickIt())
-    // remove listeners here
-
-    // }
+    document.addEventListener('keydown', handleKeyDown
+      // function clickIt(event) {
+      //   if (event.keyCode === bankOne[props.padLetter].keyCode) {
+      //     console.log('you hit ' + props.padLetter)
+      //     handleSoundPlay(props.padLetter)
+      //   }
+      // }
+    );
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- watching [] = run once
   }, [])
 
   return (
     <div className="drum-pad">
-      <button onClick={handleSoundPlay} className="drum-button">{props.padName}</button>
-      {/* this next line is useless, but required for the FCC test? */}
-      <audio id={props.padName} className="clip"><source src={bankOne[props.padName].url}></source></audio>
+      <button onClick={handleSoundPlay} className="drum-button">{props.padLetter}</button>
+      <audio id={props.padLetter} className="clip"><source src={bankOne[props.padLetter].url}></source></audio>
     </div>
   )
 }
 
 export default App;
+
+/*
+references
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+
+*/
--- src/KeybankData.js
+++ src/KeybankData.js
@@ -65,69 +65,70 @@ export const bankOne = {
 
 
 
-export const bankTwo = [
-    {
+export const bankTwo = {
+    'Q': {
         keyCode: 81,
         keyTrigger: 'Q',
         id: 'Chord-1',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
     },
 
-    {
+    'W':{
         keyCode: 87,
         keyTrigger: 'W',
         id: 'Chord-2',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
     },
 
-    {
+    'E':{
         keyCode: 69,
         keyTrigger: 'E',
         id: 'Chord-3',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
     },
 
-    {
+    'A': {
         keyCode: 65,
         keyTrigger: 'A',
         id: 'Shaker',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
     },
 
-    {
+    'S':{
         keyCode: 83,
         keyTrigger: 'S',
         id: 'Open-HH',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
     },
 
-    {
+    'D':{
         keyCode: 68,
         keyTrigger: 'D',
         id: 'Closed-HH',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
     },
 
-    {
+    'Z':{
         keyCode: 90,
         keyTrigger: 'Z',
         id: 'Punchy-Kick',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
     },
 
-    {
+    'X': {
         keyCode: 88,
         keyTrigger: 'X',
         id: 'Side-Stick',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
     },
 
-    {
+    'C':{
         keyCode: 67,
         keyTrigger: 'C',
         id: 'Snare',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-    }];
+    }
+};
 
     // let keyData = {}
 // export default keyData = {bankTwo, bankOne}
